be2d20fb58e303284012d892cdba2c32b688c7e8
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-11 16:21:33 -0400

    changed employee directory and imported mui datatables

--- react-table/src/components/EmployeeDirectory.js
+++ react-table/src/components/EmployeeDirectory.js
@@ -1,77 +1,35 @@
-import React from "react";
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import MUIDataTable from "mui-datatables";
 
-const StyledTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.color.white,
-    },
-    body: {
-        fontSize: 14,
-    },
-}))(TableCell);
+class EmployeeDirectory extends React.Component {
 
-const StyledTableRow = withStyles((theme) => ({
-    root: {
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.action.hover,
-        },
-    },
-}))(TableRow);
+  render() {
+    const columns = ["Name", "Title", "Department", "Supervisor"];
 
-function createData(name, role, DOB, hobby) {
-    return { name, role, DOB, hobby }
-}
+    const data = [
+      ['Jim Halpert', 'Salesman', 'Sales', 'Michael Scott'],
+      ['Pam Beesly', 'Receptionist', 'Assistant', 'Michael Scott'],
+      ['Dwight Schrute', 'Salesman', 'Sales', 'Michael Scott'],
+      ['Michael Scott', 'Manager', 'Manager', 'Jan Gould'],
+      ['Ryan Howard', 'Temp', 'Sales', 'Everyone else'],
+      ['Creed Bratton', 'Unknown', 'Unknown', 'Michael Scott']
+    ];
 
-const rows = [
-    createData('Jim Halpert', 'Salesman', 'Oct 20th, 1979', 'Annoy Dwight'),
-    createData('Pam Beesly', 'Receptionist', 'March 7th, 1974', 'Making art'),
-    createData('Dwight Schrute', 'Salesman', 'Jan 20th, 1966', 'Beet farm'),
-    createData('Michael Scott', 'Manager', 'Aug 16th, 1962', 'Dundies'),
-    createData('Ryan Howard', 'Temp', 'July 31st, 1979', 'Drugs'),
-    createData('Creed Bratton', 'Unknown', 'Feb 8th, 1943', 'Playing guitar')
-];
+    const options = {
+      filter: true,
+      pageLength: 5,
+      filterType: 'dropdown',
+      responsive: 'stacked',
+    };
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 700,
-    },
-});
-
-export default function EmployeeDirectory() {
-    const classes = useStyles();
-
-    return(
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Employees</StyledTableCell>
-                        <StyledTableCell align="right">Role</StyledTableCell>
-                        <StyledTableCell align="right">Date of Birth</StyledTableCell>
-                        <StyledTableCell align="right">Hobby</StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow key={row.name}>
-                            <StyledTableCell component="th" scope="row">
-                                {row.name}
-                            </StyledTableCell>
-                            <StyledTableCell align="right">{row.role}</StyledTableCell>
-                            <StyledTableCell align="right">{row.DOB}</StyledTableCell>
-                            <StyledTableCell align="right">{row.hobby}</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+    return (
+      <MUIDataTable 
+        title={"Employee Database"} 
+        data={data} 
+        columns={columns} 
+        options={options} 
+      />
     );
+  }
 }
+export default EmployeeDirectory;
